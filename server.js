400f87bd78674a743dc4e5a336f4610973d4edae
kashelton17 <kashelton17@gmail.com>
2021-06-20 13:17:11 -0400

    adding routes to server.js

--- server.js
+++ server.js
@@ -9,10 +9,18 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+app.use(express.static(__dirname + '/public/'));
+
 
 //routes
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')))
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')))
 
 app.get('/api/notes', (req, res) => res.json(JSONdb))
+
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body
+})
+
+app.listen(PORT, () => console.log(`App listening on http://localhost:${PORT}/`))
